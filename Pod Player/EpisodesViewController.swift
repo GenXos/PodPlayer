b897e06c6eeea6c4f7575ddd315b68aad404c0b1
Todd Fields <toddwfields@gmail.com>
2017-05-23 13:40:09 -0500

    UI Elements added and connected for EpisodesViewController

---
+++ Pod Player/EpisodesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EpisodesViewController.swift
+//  Pod Player
+//
+//  Created by Todd Fields on 2017-05-23.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import Cocoa
+
+class EpisodesViewController: NSViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: NSTextField!
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var pausePlayButton: NSButton!
+    @IBOutlet weak var tableView: NSTableView!
+
+    // MARK: - Initialization
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: - Functions
+    @IBAction func deleteClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func pausePlayClicked(_ sender: Any) {
+        
+        
+    }
+    
+    
+}
